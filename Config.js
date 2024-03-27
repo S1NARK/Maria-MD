ed0f8cccbf7be7fc8a560a2a005fb3ea541285a2
S1NARK <165144513+S1NARK@users.noreply.github.com>
2024-03-27 10:21:56 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['+237651755816']
+global.ownername = "FR1-M1K"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
